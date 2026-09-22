5ceafb663f23181fd043e4b9a45428c895a65cf7
patelayush9876 <patelayush9876@gmail.com>
2026-09-22 22:26:28 +0530

    Updated app name

--- vite.config.ts
+++ vite.config.ts
@@ -86,7 +86,7 @@ function figmaSiteConfiguration(config: FigmaSiteConfiguration): Plugin {
     return html.replace(`<!-- ${slotName} -->`, content)
   }
 
-  const title = config.title ?? "Figma Make App"
+  const title = config.title ?? "Happy Diwali"
   const description = config.description ?? ''
   const favicon = config.icons?.icon ?? ''
   const socialImage = config.openGraph?.image ?? ''
